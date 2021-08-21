17ccee38115d2f001a4860e5a76d901d6fc2bcc5
AjaniSt <74562116+AjaniSt@users.noreply.github.com>
2021-08-20 23:37:04 -0400

    Attempted fix
    Tried to fix line 190

--- chartjs-plugin-watermark.js
+++ chartjs-plugin-watermark.js
@@ -35,7 +35,7 @@
 var Chart = require('chart.js');
 Chart = typeof(Chart) === 'function' ? Chart : window.Chart;
 
-var watermarkPlugin = {
+let watermarkPlugin = {
 
     defaultOptions: {
         x: 0,
